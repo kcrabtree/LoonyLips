4aa15eb4aad3e7096326b98759559d02107219ed
Keith Crabtree <keithcrabtree@live.com>
2020-05-10 22:50:05 -0400

    section 1.13 completed

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -4,7 +4,5 @@ extends Control
 func _ready():
 	var prompts = ["Yan", "Minions", "greatest", "two"]
 	var story = "Once upon a time %s watched %s and thought it was the %s movie of the past %s decades."
-	print(story % prompts)
 	
-	prompts = ["Keith", "Minions", "worst", "ten"]
-	print(story % prompts)
+	$DisplayText.text = story % prompts
